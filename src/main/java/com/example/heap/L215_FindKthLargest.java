885e9825a01e3fa197c0c7989016466c30e8644a
zhenwu <zhenwu@gmail.com>
2024-08-04 21:58:15 +0800

    L215_数组中的第K个最大元素

---
+++ src/main/java/com/example/heap/L215_FindKthLargest.java
@@ -0,0 +1,26 @@
+package com.example.heap;
+
+import java.util.PriorityQueue;
+
+/**
+ * <p>L215:数组中的第K个最大元素</p>
+ * @author zhenwu
+ * @date 2024/8/4 21:48
+ */
+public class L215_FindKthLargest {
+    public static void main(String[] args) {
+
+    }
+
+    private static int findKthLargest(int[] nums, int k) {
+        PriorityQueue<Integer> heap = new PriorityQueue<>(k);
+        for (int num : nums) {
+            heap.add(num);
+            if (heap.size() > k) {
+                // 移除前面比第k个数小的 k - 1个数
+                heap.poll();
+            }
+        }
+        return heap.peek();
+    }
+}
