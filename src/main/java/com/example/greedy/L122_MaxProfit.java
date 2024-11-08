66a4f9bbd1dbffa9bb4ba86e5fe9a69642d547f7
zhenwu <zhenwu@gmail.com>
2024-11-08 20:17:16 +0800

    L121_买卖股票的最佳时机<

--- src/main/java/com/example/greedy/L122_MaxProfit.java
+++ src/main/java/com/example/greedy/L122_MaxProfit.java
@@ -11,6 +11,34 @@ public class L122_MaxProfit {
 
     }
 
+    /**
+     * 动态规划解法
+     * dp[i][j]表示第i天处于状态j时的最大利润  j=0表示持有股票，j=1表示不持有股票
+     * dp[i][0] = max(dp[i-1][0], dp[i-1][1] - prices[i])
+     * dp[i][1] = max(dp[i-1][1], dp[i-1][0] + prices[i])
+     * 时间复杂度：O(n)
+     * 空间复杂度：O(n)
+     */
+    private static int maxProfitPro(int[] prices) {
+        if (prices.length < 2) {
+            return 0;
+        }
+        int n = prices.length;
+        int[][] dp = new int[n][2];
+        dp[0][0] = -prices[0];
+        dp[0][1] = 0;
+        for (int i = 1; i < n; i++) {
+            dp[i][0] = Math.max(dp[i - 1][0], dp[i - 1][1] - prices[i]);
+            dp[i][1] = Math.max(dp[i - 1][1], dp[i - 1][0] + prices[i]);
+        }
+        return dp[n - 1][1];
+    }
+
+    /**
+     * 贪心解法
+     * 时间复杂度：O(n)
+     * 空间复杂度：O(1)
+     */
     private static int maxProfit(int[] prices) {
         if (prices == null || prices.length < 2) {
             return 0;
