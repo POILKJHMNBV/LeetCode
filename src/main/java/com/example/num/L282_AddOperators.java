5a08c4f2d8bccf2d908dcf9184b29c63b34abd88
zhenwu <zhenwu@gmail.com>
2024-11-28 21:44:27 +0800

    L282_给表达式添加运算符

---
+++ src/main/java/com/example/num/L282_AddOperators.java
@@ -0,0 +1,71 @@
+package com.example.num;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * <p>L282:给表达式添加运算符</p>
+ * <p>给定一个仅包含数字 0-9 的字符串 num 和一个目标值整数 target ，在 num 的数字之间添加 二元 运算符（不是一元）+、- 或 * ，返回 所有 能够得到 target 的表达式。</p>
+ * @author zhenwu
+ * @date 2024/11/28 21:37
+ */
+public class L282_AddOperators {
+
+    public static void main(String[] args) {
+        String num = "123";
+        int target = 6;
+        List<String> result = new L282_AddOperators().addOperators(num, target);
+        for (String s : result) {
+            System.out.println(s);
+        }
+    }
+
+    int n;
+    String num;
+    int target;
+    List<String> ans;
+
+    public List<String> addOperators(String num, int target) {
+        this.n = num.length();
+        this.num = num;
+        this.target = target;
+        this.ans = new ArrayList<String>();
+        StringBuffer expr = new StringBuffer();
+        backtrack(expr, 0, 0, 0);
+        return ans;
+    }
+
+    /**
+     * 时间复杂度：O(4^n)
+     * 空间复杂度：O(n)
+     */
+    public void backtrack(StringBuffer expr, int i, long res, long mul) {
+        if (i == n) {
+            if (res == target) {
+                ans.add(expr.toString());
+            }
+            return;
+        }
+        int signIndex = expr.length();
+        if (i > 0) {
+            expr.append(0); // 占位，下面填充符号
+        }
+        long val = 0;
+        // 枚举截取的数字长度（取多少位），注意数字可以是单个 0 但不能有前导零
+        for (int j = i; j < n && (j == i || num.charAt(i) != '0'); ++j) {
+            val = val * 10 + num.charAt(j) - '0';
+            expr.append(num.charAt(j));
+            if (i == 0) { // 表达式开头不能添加符号
+                backtrack(expr, j + 1, val, val);
+            } else { // 枚举符号
+                expr.setCharAt(signIndex, '+');
+                backtrack(expr, j + 1, res + val, val);
+                expr.setCharAt(signIndex, '-');
+                backtrack(expr, j + 1, res - val, -val);
+                expr.setCharAt(signIndex, '*');
+                backtrack(expr, j + 1, res - mul + mul * val, mul * val);
+            }
+        }
+        expr.setLength(signIndex);
+    }
+}
