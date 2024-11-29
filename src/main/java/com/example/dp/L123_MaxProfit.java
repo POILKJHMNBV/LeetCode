314a7624bf05e90f4f3cdd7bd3564282a677611e
zhenwu <zhenwu@gmail.com>
2024-11-29 21:07:17 +0800

    L123_买卖股票的最佳时机III

--- src/main/java/com/example/dp/L123_MaxProfit.java
+++ src/main/java/com/example/dp/L123_MaxProfit.java
@@ -15,6 +15,25 @@ public class L123_MaxProfit {
     }
 
     /**
+     * 时间复杂度：O(n)
+     * 空间复杂度：O(1)
+     */
+    private static int maxProfitPro(int[] prices) {
+        int len = prices.length;
+        if (len == 1) {
+            return 0;
+        }
+        int a = -prices[0], b = 0, c = -prices[0], d = 0;
+        for (int i = 1; i < len; i++) {
+            d = Math.max(d, c + prices[i]);
+            c = Math.max(c, b - prices[i]);
+            b = Math.max(b, a + prices[i]);
+            a = Math.max(a, -prices[i]);
+        }
+        return d;
+    }
+
+    /**
      * 0 - 第一次持有股票     1 - 第一次不持有股票     2 - 第二次持有股票     3 - 第二次不持有股票
      * dp[i][j]: 第i天处于j状态所能获得的最大收益
      * 递推公式：
@@ -22,6 +41,8 @@ public class L123_MaxProfit {
      * dp[i][1] = max(dp[i - 1][1], dp[i - 1][0] + prices[i])
      * dp[i][2] = max(dp[i - 1][2], dp[i - 1][1] - prices[i])
      * dp[i][3] = max(dp[i - 1][3], dp[i - 1][2] + prices[i])
+     * 时间复杂度：O(n)
+     * 空间复杂度：O(n)
      */
     private static int maxProfit(int[] prices) {
         int len = prices.length;
