697c49cd159374c4f4e5b5ec05b0d905f3db5f53
zhenwu <zhenwu@gmail.com>
2024-12-09 21:18:10 +0800

    L217_存在重复元素 II

--- src/main/java/com/example/slidewindow/L219_ContainsNearbyDuplicate.java
+++ src/main/java/com/example/slidewindow/L219_ContainsNearbyDuplicate.java
@@ -1,6 +1,8 @@
 package com.example.slidewindow;
 
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Map;
 import java.util.Set;
 
 /**
@@ -13,6 +15,42 @@ public class L219_ContainsNearbyDuplicate {
         int[] nums = {1, 2, 3, 1, 2, 3};
         int k = 2;
         System.out.println(containsNearbyDuplicate(nums, k));
+    }
+
+    /**
+     * 滑动窗口 + 哈希表
+     * 时间复杂度：O(n)
+     * 空间复杂度：O(min(k, n))
+     */
+    private static boolean containsNearbyDuplicatePlus(int[] nums, int k) {
+        Set<Integer> set = new HashSet<>();
+        int len = nums.length;
+        for (int i = 0; i < len; i++) {
+            if (i > k) {
+                set.remove(nums[i - k - 1]);
+            }
+            if (!set.add(nums[i])) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /**
+     * 哈希表
+     * 时间复杂度：O(n)
+     * 空间复杂度：O(n)
+     */
+    private static boolean containsNearbyDuplicatePro(int[] nums, int k) {
+        Map<Integer, Integer> map = new HashMap<>();
+        int len = nums.length;
+        for (int i = 0; i < len; i++) {
+            if (map.containsKey(nums[i]) && i - map.get(nums[i]) <= k) {
+                return true;
+            }
+            map.put(nums[i], i);
+        }
+        return false;
     }
 
     /**
