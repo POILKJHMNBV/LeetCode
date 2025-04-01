2e333564f898f62b553e5d1266d519427da35d93
zhenwu <zhenwu@gmail.com>
2025-04-01 22:10:12 +0800

    L744_寻找比目标字母大的最小字母

---
+++ src/main/java/com/example/binaryserach/L744_NextGreatestLetter.java
@@ -0,0 +1,29 @@
+package com.example.binaryserach;
+
+/**
+ * <p>L744:寻找比目标字母大的最小字母</p>
+ * @author zhenwu
+ * @date 2025/4/1 22:08
+ */
+public class L744_NextGreatestLetter {
+    public static void main(String[] args) {
+
+    }
+
+    private static char nextGreatestLetter(char[] letters, char target) {
+        int length = letters.length;
+        if (target >= letters[length - 1]) {
+            return letters[0];
+        }
+        int low = 0, high = length - 1;
+        while (low < high) {
+            int mid = (high - low) / 2 + low;
+            if (letters[mid] > target) {
+                high = mid;
+            } else {
+                low = mid + 1;
+            }
+        }
+        return letters[low];
+    }
+}
