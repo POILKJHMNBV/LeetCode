a570a77db50e5335c1b65d5fb06e64d789398aee
zhenwu <zhenwu0601@gmail.com>
2025-08-08 22:13:05 +0800

    L802_找到最终的安全状态

---
+++ src/main/java/com/example/dfs/L802_EventualSafeNodes.java
@@ -0,0 +1,61 @@
+package com.example.dfs;
+
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Queue;
+
+/**
+ * <p>L802:找到最终的安全状态</p>
+ * @author zhenwu
+ * @date 2025/8/8 20:50
+ */
+public class L802_EventualSafeNodes {
+    public static void main(String[] args) {
+        int[][] graph = {{1, 2, 3, 4}, {1, 2}, {3, 4}, {0, 4}, {}};
+        System.out.println(eventualSafeNodes(graph));
+    }
+
+    /**
+     * 时间复杂度：O(n + m)
+     * 空间复杂度：O(n + m)
+     */
+    private static List<Integer> eventualSafeNodes(int[][] graph) {
+        List<Integer> safeList = new ArrayList<>();
+        int n = graph.length;
+        int[] outdegrees = new int[n];
+        List<Integer>[] prevArr = new List[n];
+        for (int i = 0; i < n; i++) {
+            prevArr[i] = new ArrayList<Integer>();
+        }
+        for (int i = 0; i < n; i++) {
+            int[] adjacent = graph[i];
+            outdegrees[i] = adjacent.length;
+            for (int next : adjacent) {
+                prevArr[next].add(i);
+            }
+        }
+        Queue<Integer> queue = new ArrayDeque<Integer>();
+        for (int i = 0; i < n; i++) {
+            if (outdegrees[i] == 0) {
+                queue.offer(i);
+            }
+        }
+        while (!queue.isEmpty()) {
+            int node = queue.poll();
+            List<Integer> prevList = prevArr[node];
+            for (int prev : prevList) {
+                outdegrees[prev]--;
+                if (outdegrees[prev] == 0) {
+                    queue.offer(prev);
+                }
+            }
+        }
+        for (int i = 0; i < n; i++) {
+            if (outdegrees[i] == 0) {
+                safeList.add(i);
+            }
+        }
+        return safeList;
+    }
+}
