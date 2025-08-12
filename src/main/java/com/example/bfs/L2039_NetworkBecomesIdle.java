d16e716d156880acf9ce4fd8c1b3a140ddffae91
zhenwu <zhenwu0601@gmail.com>
2025-08-12 22:15:25 +0800

    L2039_网络空闲的时刻

---
+++ src/main/java/com/example/bfs/L2039_NetworkBecomesIdle.java
@@ -0,0 +1,54 @@
+package com.example.bfs;
+
+import java.util.*;
+
+/**
+ * <p>L2039:网络空闲的时刻</p>
+ * @author zhenwu
+ * @date 2025/8/12 21:44
+ */
+public class L2039_NetworkBecomesIdle {
+    public static void main(String[] args) {
+
+    }
+
+    /**
+     * 时间复杂度：O(n + m)
+     * 空间复杂度：O(n + m)
+     */
+    private static int networkBecomesIdle(int[][] edges, int[] patience) {
+        int n = patience.length;
+        List<Integer>[] adj = new List[n];
+        for (int i = 0; i < n; ++i) {
+            adj[i] = new ArrayList<Integer>();
+        }
+        boolean[] visit = new boolean[n];
+        for (int[] v : edges) {
+            adj[v[0]].add(v[1]);
+            adj[v[1]].add(v[0]);
+        }
+
+        Queue<Integer> queue = new ArrayDeque<Integer>();
+        queue.offer(0);
+        visit[0] = true;
+        int dist = 1;
+        int ans = 0;
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            for (int i = 0; i < size; i++) {
+                int curr = queue.poll();
+                for (int v : adj[curr]) {
+                    if (visit[v]) {
+                        continue;
+                    }
+                    queue.offer(v);
+                    int time = patience[v] * ((2 * dist - 1) / patience[v]) + 2 * dist + 1;
+                    ans = Math.max(ans, time);
+                    visit[v] = true;
+                }
+            }
+            dist++;
+        }
+        return ans;
+    }
+}
